fc3e3449a0a804af72ae2ddef50565d6b430229e
warlockz <yogerocks@gmail.com>
2017-04-19 23:55:24 +0530

    Assignments

--- hckrnk/cpp-variadics.cpp
+++ hckrnk/cpp-variadics.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 // Enter your code for reversed_binary_value<bool...>()
+//1st solution
 static int count = 0;
 template<bool... digits>
 int reversed_binary_value()
@@ -9,7 +10,7 @@ int reversed_binary_value()
     bool valarr[] = {digits...};
     int size = sizeof...(digits);
     int temp = 1;
-    cout<<"\n start : " << count++ <<"\n \n";
+    cout<<"\n reversed_binary_value start : " << count++ <<"\n \n";
     for(int i = 0; i < size; ++i)
     {
         cout<<valarr[i]<<" ";
@@ -29,11 +30,47 @@ int reversed_binary_value()
     cout << "\n  \n";
     return z;
 }
+//1st solution
+/*
+//2nd solution
+// Enter your code for reversed_binary_value<bool...>()
+template<bool... digits>
+struct BitSolver;
+
+template<bool bit,bool... digits>
+struct BitSolver<bit,digits...>
+{
+    static int reversed_bit_solver()
+    {
+        return (bit + 2*BitSolver<digits...>::reversed_bit_solver());
+
+    }
+};
+
+template<>
+struct BitSolver<>
+{
+    static int reversed_bit_solver()
+    {
+        return 0;
+    }
+};
+
+template<bool... digits>
+int reversed_binary_value()
+{ 
+    return (BitSolver<digits...>::reversed_bit_solver());
+}
+
+
+//2nd solution
+*/
 
 template <int n, bool...digits>
 struct CheckValues {
   	static void check(int x, int y)
   	{
+      cout<<"\n check n-1 start : " << count++ <<"\n \n";
     	CheckValues<n-1, 0, digits...>::check(x, y);
     	CheckValues<n-1, 1, digits...>::check(x, y);
   	}
@@ -43,6 +80,7 @@ template <bool...digits>
 struct CheckValues<0, digits...> {
   	static void check(int x, int y)
   	{
+      cout<<"\n check() start : " << count++ <<"\n \n";
     	int z = reversed_binary_value<digits...>();
     	std::cout << (z+64*y==x);
   	}
